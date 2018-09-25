3ae98da2eba0d6156e897e79a8acd5a0f0fcb762
Dmitry Skripka <skripka@zfort.local>
2018-09-25 14:42:25 +0300

    Init commit

---
+++ Test1/SumbitAgencyStep1.cs
@@ -0,0 +1,99 @@
+﻿using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Support.Events;
+using OpenQA.Selenium.Support.PageObjects;
+using System.Threading.Tasks;
+using System;
+
+
+namespace Test1
+{
+    public class SumbitAgencyStep1
+    {
+        private readonly IWebDriver driver;
+        public WebDriverWait wait;
+
+        private readonly string url = @"http://designrush.devplatform2.com/submit/agency/step/1";
+
+        public SumbitAgencyStep1(IWebDriver browser)
+        {
+            this.driver = browser;
+            this.driver.Manage().Window.Maximize();
+            PageFactory.InitElements(browser, this);
+        }
+
+        [FindsBy(How = How.Name, Using = "first_name")]
+        public IWebElement nameField { get; set; }
+
+        [FindsBy(How = How.Name, Using = "last_name")]
+        public IWebElement lastNameField { get; set; }
+
+        [FindsBy(How = How.Name, Using = "email")]
+        public IWebElement emailField { get; set; }
+
+        [FindsBy(How = How.Name, Using = "phone")]
+        public IWebElement phoneField { get; set; }
+
+        [FindsBy(How = How.Name, Using = "password")]
+        public IWebElement passwordField { get; set; }
+
+        [FindsBy(How = How.Name, Using = "password_confirmation")]
+        public IWebElement passwordConfirmationField { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "step-btn")]
+        public IWebElement submitButton { get; set; }
+
+
+        public void Navigate()
+        {
+            this.driver.Navigate().GoToUrl(this.url);
+        }
+
+        public void Submit(string name, string lastname, string email, string phone, string password, string passwordConfirmation)
+        {
+            this.nameField.Clear();
+            this.nameField.SendKeys(name);
+
+            this.lastNameField.Clear();
+            this.lastNameField.SendKeys(lastname);
+
+            this.emailField.Clear();
+            this.emailField.SendKeys(email);
+
+            this.phoneField.Clear();
+            this.phoneField.SendKeys(phone);
+
+            this.emailField.Clear();
+            this.passwordField.SendKeys(password);
+
+            this.passwordConfirmationField.Clear();
+            this.passwordConfirmationField.SendKeys(passwordConfirmation);
+
+            this.submitButton.Click();
+        }
+
+        //public void iSSubmitSuccessful(bool isElementfound)
+        //{
+
+
+        //    if (assertCondition != null)
+        //    {
+        //        isElementfound = true;
+        //    }
+        //    else
+        //    {
+        //        isElementfound = false;
+        //    }
+        //    Assert.IsNotNull(isElementfound);
+        //}
+
+    }
+
+
+
+}
---
+++ Test1/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Support.Events;
+using System;
+
+
+namespace Test1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        public IWebDriver driver { get; set; }
+        public WebDriverWait wait { get; set; }
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            this.driver = new ChromeDriver();
+            this.wait = new WebDriverWait(this.driver, TimeSpan.FromSeconds(30));
+        }
+
+        [TestCleanup]
+        public void TeardownTest()
+        {
+            this.driver.Quit();
+        }
+
+        [TestMethod]
+        public void SubmitStep1()
+        {
+            SumbitAgencyStep1 step1 = new SumbitAgencyStep1(this.driver);
+            step1.Navigate();
+            step1.Submit("DmitryQaTest123", "SkripkaQaTest123", @"dimaskripka1992+qwe123qwe123@gmail.com", @"+380932967718", @"Qwerty123!", @"Qwerty123!");
+            driver.FindElements(By.ClassName("alertify-button"));
+        }
+    }
+}
